5ccaabcc783f1136fbf1342336f1b212f8c44d54
Nicolás Miño <nmino@invico.gov.ar>
2018-02-01 12:38:46 -0300

    Agregamos y modificamos modulos

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+
+// Modulos
+import { SharedModule } from '../shared/shared.module';
+
+// Components
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+// Rutas
+import { PAGES_ROUTES } from './pages.routes';
+
+
+
+@NgModule({
+    declarations: [
+        PagesComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    exports: [
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component,
+        PagesComponent,
+    ],
+    imports: [
+        SharedModule,
+        PAGES_ROUTES
+    ]
+})
+
+export class PagesModule {
+
+}
